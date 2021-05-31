81f6d3b7579eb1d1455eec05aeb996e01154b71e
BrayanLemusQ <brayan.lemus.q@gmail.com>
2021-05-31 17:54:36 -0500

    Explicación de Renderización de pantalla

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def homepage():
-    return "<h1>Bienvenidos!<h1>"
+    return "<h1>Welcome!<h1>"
 
 if __name__=='main':
     app.run(debug=True)
