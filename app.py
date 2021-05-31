43c63f73f1b1a214aa80204fb587e23ea5671d7b
BrayanLemusQ <brayan.lemus.q@gmail.com>
2021-05-31 17:45:57 -0500

    Renderización de Pantalla

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def homepage():
-    return "Virtual Environment and Flask Running"
+    return "<h1>Bienvenidos!<h1>"
 
 if __name__=='main':
     app.run(debug=True)
