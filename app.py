b34806ec0371a97ef9e81ae43a245964f469e83c
BrayanLemusQ <brayan.lemus.q@gmail.com>
2021-05-31 18:14:06 -0500

    Renderización y uso de plantillas o templates

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def homepage():
-    return "<h1>Welcome!<h1>"
+    return render_template("index.html")
+
+@app.route('/goodbye')
+def goodbyepage():
+    return "<h1>Goodbye!<h1>"
 
 if __name__=='main':
     app.run(debug=True)
