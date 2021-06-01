0fccaf429df229536b3948b0641e7dc8234b6b47
BrayanLemusQ <brayan.lemus.q@gmail.com>
2021-06-01 14:30:49 -0500

    Se agregan nuevos templates con nueva interfaz, se añade la ruta edit

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from app import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from models import Task
+
 import forms
 
 
@@ -21,5 +22,23 @@ def add():
         task = Task(title=form.title.data)
         db.session.add(task)
         db.session.commit()
+        flash('Task added to database')
         return redirect(url_for("homepage"))
     return render_template("add.html", form=form)
+
+@app.route('/edit<int:task_id>', methods=["POST", "GET"])
+def edit(task_id):
+    task = Task.query.get(task_id)
+    form = forms.AddTaskForm()
+
+    if task:
+        if form.validate_on_submit():
+            task.title = form.title.data
+            db.session.commit()
+            flash('The task has beeen updated')
+            return redirect(url_for("homepage"))
+        form.title.data = task.title
+        return render_template("edit.html", form=form, task_id=task_id)
+    else:
+        flash('The task was not found')
+        return redirect(url_for("homepage"))
