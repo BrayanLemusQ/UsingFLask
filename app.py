2a54aecadc567f9b1e9e79df570a14f2b5dc0e62
BrayanLemusQ <brayan.lemus.q@gmail.com>
2021-06-01 11:21:45 -0500

    Se crea una base de datos con SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config["SECRET_KEY"] = "PASSWORD"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
+
 from routes import *
 
 if __name__=='main':
---
+++ models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+
+def __repr__(self):
+    return f'{self.title}'
