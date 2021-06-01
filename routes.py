5c38fe89e8f473f2717e748deebf5f5cf1f0a201
BrayanLemusQ <brayan.lemus.q@gmail.com>
2021-05-31 23:14:11 -0500

    Capturar valor del campo y postearlo en la pagina

--- routes.py
+++ routes.py
@@ -12,7 +12,9 @@ def homepage():
 def goodbyepage():
     return render_template("goodbye.html", name="Brayan Lemus")
 
-@app.route('/add')
+@app.route('/add', methods=["POST", "GET"])
 def add():
     form = forms.AddTaskForm()
+    if form.validate_on_submit():
+        return render_template("add.html", form=form, title=form.title.data)
     return render_template("add.html", form=form)
